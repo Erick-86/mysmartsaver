547681b0bcba0540f41e5a8f70a99c1861cbd625
FreeMan <zelonaire@gmail.com>
2025-01-07 11:12:19 +0000

    saving logic

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ frequency = input("How would you like to save:\n 1. Daily\n 2. Weekly\n 3. Month
 amount_to_save = int(input(f"How much would you like to save {frequency.lower()}: "))
 period_goal = str(input("Do you have a period, or a goal to reach:\n 1. Period\n 2. Goal").lower())
 
+# goal
+# period
+period = 0
+goal = 0
+
 if period_goal == "period" or period_goal == "1":
     if frequency == "Daily" or frequency == "1":
         period = int(input("How many days would you like to save: "))
@@ -14,12 +19,22 @@ if period_goal == "period" or period_goal == "1":
         period = int(input("How many months would you like to save: "))
     else: 
         print("Invalid input")
-elif period_goal == "goal" or period_goal == "3":
+elif period_goal == "goal" or period_goal == "2":
     goal = int(input("How much is your saving goal: "))
 else: 
     print("Invalid input")
 
 amount_saved = 0
-goal_reached = 0
 period_time = 0
 
+frequencies = ["Day", "Week", "Month"]
+
+currnt_save = amount_to_save
+
+# Saving logic
+while (period_time < period and period > 0) or (amount_saved < goal and goal > 0):
+    period_time += 1
+    amount_saved += currnt_save
+
+    print(f" {frequencies[0]} {period_time}: Save ${currnt_save} (Total = {amount_saved})")
+    currnt_save += 1
